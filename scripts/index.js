e3c6edb144a0b4cbe46862e9a148bd4e8b214e01
TejoVarma <tejovarma.alluri9@gmail.com>
2023-05-20 11:11:23 +0530

    Created a folder structure from the past files and committed

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+function getData() {
+    let search = document.getElementById("searchBar").value;
+    let content = document.getElementById("content");
+    if (!search) {
+        return alert("Enter the movie name...");
+    }
+    content.innerHTML = "";
+    let loaderDiv = document.createElement("div");
+    loaderDiv.id = "loader";
+    content.append(loaderDiv);
+    fetch(`https://www.omdbapi.com/?t=${search}&apikey=18c03f0c`)
+        .then((res) => res.json())
+        .then((result) => {
+            if (result.Response !== "True") {
+                content.innerHTML = "No such movie exist!";
+            }
+            else {
+                let dataRequired = ["Title", "Year", "Rated", "Director", "Writer", "Actors"];
+                let img = document.createElement("img");
+                img.src = result.Poster;
+                img.alt = "Img not available";
+                let div = document.createElement("div");
+                let reqContents = "";
+                for (let i = 0; i < dataRequired.length; i++) {
+                    reqContents += "<b>" + dataRequired[i] + "</b>" + ":" + result[dataRequired[i]] + "<br>";
+                }
+                content.innerHTML = "";
+                div.innerHTML = reqContents;
+                content.append(img);
+                content.append(div);
+            }
+        })
+}
